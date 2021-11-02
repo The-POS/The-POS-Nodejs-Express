d3eb2e33828cc257e797c2385656bc63e54bbdb0
Abdelrahman Saed <me@abdosaed.ml>
2021-11-02 20:59:14 +0200

    add frequently used middlewares and remove body-parser

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const middlewares = require('./middlewares/index');
 const multParse = require('./middlewares/multer');
 
 const app = express();
 
-app.use(bodyParser.json()); // application/json
-app.use(bodyParser.urlencoded({ extended: true })); // x-www-form-urlencoded
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // disable CROS for all websites
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST , PUT, DELETE'); //  Specify which methods allowed
-  res.setHeader('Access-Control-Allow-Headers', 'Content-type , Authorization');
-  next();
-});
+middlewares(app);
 
 app.use((error, req, res, next) => {
   console.log(error);
